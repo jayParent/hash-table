6daf9a958b709b361b37e170db5b66ad20606130
Jerome <jeromeparent9@gmail.com>
2021-04-20 17:26:45 -0400

    add c# version

---
+++ csharp/HashTable.cs
@@ -0,0 +1,22 @@
+public class HashTable
+{
+    public ulong[] Table { get; set; }
+    public int Size { get; set; } = 1000;
+
+    public HashTable()
+    {
+        Table = new ulong[Size];
+    }
+    private void Hash(){
+
+    }
+    private void Add(){
+
+    }
+    private void Find(){
+
+    }
+    private void Delete(){
+        
+    }
+}
