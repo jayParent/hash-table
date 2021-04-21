ac31ccc0c124cf9a79ae347f64851bd331ce0910
Jerome <jeromeparent9@gmail.com>
2021-04-20 22:19:00 -0400

    Add and Find with checksum hash

--- csharp/HashTable.cs
+++ csharp/HashTable.cs
@@ -1,22 +1,90 @@
+using System;
+using System.Collections.Generic;
+
 public class HashTable
 {
-    public ulong[] Table { get; set; }
+    public List<string>[] Table { get; set; }
     public int Size { get; set; } = 1000;
 
     public HashTable()
     {
-        Table = new ulong[Size];
+        Table = new List<string>[Size];
     }
-    private void Hash(){
+    private ulong Hash(char[] buffer, uint n)
+    {
+        ulong sum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            sum += buffer[i];
+        }
+
+        return sum;
+    }
+    public void Add(string word)
+    {
+        char[] buffer = word.ToCharArray();
+        uint n = (uint)buffer.Length;
+        ulong h = Hash(buffer, n);
+
+        if (Table[h] == null)
+        {
+            Table[h] = new List<string>();
+            Table[h].Add(word);
+        }
+        else
+        {
+            Table[h].Add(word);
+        }
 
     }
-    private void Add(){
+    public List<int[]> Find(string word)
+    {
+        char[] buffer = word.ToCharArray();
+        uint n = (uint)buffer.Length;
+        ulong h = Hash(buffer, n);
+        List<int[]> position = GetPositions(word, h);
+
+        return position;
 
     }
-    private void Find(){
+
+    private List<int[]> GetPositions(string word, ulong h)
+    {
+        int tablePosition = (int)h;
+        int listPosition = 0;
+        List<int[]> positions = new List<int[]>();
+
+        for (int i = 0; i < Table[tablePosition].Count; i++)
+        {
+            if (Table[tablePosition][i] == word)
+            {
+                int[] position = new int[2];
+                listPosition = i;
+                position[0] = tablePosition;
+                position[1] = listPosition;
+                positions.Add(position);
+            }
+        }
+
+        return positions;
+    }
+
+    public void Delete()
+    {
 
     }
-    private void Delete(){
-        
+    public void ShowTableInfo()
+    {
+        int filledSlots = 0;
+
+        for (int i = 0; i < Table.Length; i++)
+        {
+            if (Table[i] != null)
+                filledSlots++;
+        }
+
+        Console.WriteLine($"{filledSlots}");
+
     }
 }
--- csharp/Program.cs
+++ csharp/Program.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace csharp
 {
     class Program
     {
+        public static Dictionary<string, string> Prompts = new Dictionary<string, string>
+        {
+            {"operations", "1: Ajouter\n2: Rechercher\n3: Supprimer\n4: Quitter\n"},
+            {"word", "Entrez le mot: "}
+        };
+        private static StreamReader Dictionnaire = new StreamReader("dictionnaire.txt");
+
         static void Main(string[] args)
         {
+            FillWithDict();
+        }
 
+        private static void Run()
+        {
+            HashTable hashTable = new HashTable();
+            FillWithDict();
+
+            while (true)
+            {
+                Console.WriteLine($"{Prompts["operations"]}");
+                string input = Console.ReadLine();
+
+                switch (input)
+                {
+                    case "1":
+                        Console.Write($"{Prompts["word"]}");
+                        string word = Console.ReadLine();
+                        hashTable.Add(word);
+
+                        break;
+
+                    case "4":
+                        break;
+                }
+
+                break;
+            }
+        }
+        private static void FillWithDict()
+        {
+            string word;
+            
+            HashTable hashTable = new HashTable();
+
+            while ((word = Dictionnaire.ReadLine()) != null)
+            {
+                hashTable.Add(word);
+            }
+
+            hashTable.Add("aa");
+            Dictionnaire.Close();
+            var positions = hashTable.Find("aa");
+            hashTable.ShowTableInfo();
         }
     }
 }
